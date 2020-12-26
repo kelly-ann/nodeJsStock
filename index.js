17bfe178482cf185039bf20ce6c6684b9cc170dc
kelly-ann <kapeart@gmail.com>
2020-12-26 15:54:44 -0500

    Connected the form to the API

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
-// Stock Market Portfolio App By K-A
+// Stock Symbol Lookup Node JS App By K-A
 
 const express = require('express');
 const app = express();
 const exphbs  = require('express-handlebars');
 const path = require('path');
 const request = require('request');
+const bodyParser = require('body-parser');
 
 // this line says that when you spin up the none web server you should listen on 
 // whatever the environment's default port is set to OR if you cannot then listen on port 5000 instead.
 const PORT = process.env.PORT || 5000;
 
+// use body-parser middleware
+app.use(bodyParser.urlencoded({extended: false}));
+
 // create callApi function
-function callApi(finishedApi) {
-	request('https://cloud.iexapis.com/stable/stock/rds.b/quote?token=pk_a6555f075d9d46bfa4582fd0d023e1a9',
+function callApi(finishedApi, tickerSymbol) {
+	request('https://cloud.iexapis.com/stable/stock/' + tickerSymbol + '/quote?token=pk_a6555f075d9d46bfa4582fd0d023e1a9',
 		{ json: true }, 
 		(err, res, body) => {
 			console.log(" **** NEW RUN **** ");
@@ -27,20 +31,29 @@ function callApi(finishedApi) {
 	);
 }
 
-
 // set Handlebars middleware
 app.engine('handlebars', exphbs()); 		//use the engine to Handlebars which is an instance of exphbs
 app.set('view engine', 'handlebars');		//set view engine to Handlebars
 
-const otherStuff = "Hi there, this is other stuff!";
-
-// set Handlebars routes
+// set Handlebars index.html GET route
 app.get('/', function (req, res) {
 	callApi(function(doneWithApi) {
 		res.render('home', {
 	    	stock: doneWithApi
 	    });	
-	});
+	}, "rds.b");
+    
+});
+
+// set Handlebars index.html POST route
+app.post('/', function (req, res) {
+	callApi(function(doneWithApi) {
+		//postedTextFromForm = req.body.stockSymbolInput;
+		res.render('home', {
+	    	stock: doneWithApi,
+	    	//postedSymbol: postedTextFromForm
+	    });	
+	}, req.body.stockSymbolInput);
     
 });
 
@@ -52,6 +65,5 @@ app.get('/about.html', function (req, res) {
 // set static folder
 // this is how you create a "route" in using Express JS for static (NOT dynamic) files.
 app.use(express.static(path.join(__dirname, 'public')));
-
 
 app.listen(PORT, () => console.log('**** Server listening on port: ' + PORT));
